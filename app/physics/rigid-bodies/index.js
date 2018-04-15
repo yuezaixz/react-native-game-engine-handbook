14f3741c98f140cfe30886c397e21864d4f53779
吴迪玮 <xiao303178394@gmail.com>
2018-04-15 09:23:26 +0800

    vscode 配置

--- app/physics/rigid-bodies/index.js
+++ app/physics/rigid-bodies/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
-import {StatusBar, Dimensions} from "react-native";
-import {GameEngine} from "react-native-game-engine";
-import {Physics, CreateBox, MoveBox, CleanBoxes} from "./systems";
-import {Box,Cloth} from "./renderers";
+import React, { Component } from "react";
+import { StatusBar, Dimensions } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import { Physics, CreateBox, MoveBox, CleanBoxes } from "./systems";
+import { Box, Cloth } from "./renderers";
 import Matter from "matter-js";
 
 Matter.Common.isElement = () => false; //-- Overriding this function because the original references HTMLElement
@@ -13,21 +13,42 @@ export default class RigidBodies extends Component {
     }
 
     render() {
-        const {width, height} = Dimensions.get("window");
+        const { width, height } = Dimensions.get("window");
         const boxSize = Math.trunc(Math.max(width, height) * 0.075);
 
-        const engine = Matter.Engine.create({enableSleeping: false});
+        const engine = Matter.Engine.create({ enableSleeping: false });
         const world = engine.world;
-        const body = Matter.Bodies.rectangle(width / 2, -1000, boxSize, boxSize, {frictionAir: 0.021, restitution: 1});
-        const floor = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, {
+        const body = Matter.Bodies.rectangle(
+            width / 2,
+            -1000,
+            boxSize,
+            boxSize,
+            { frictionAir: 0.021, restitution: 1 }
+        );
+        const floor = Matter.Bodies.rectangle(
+            width / 2,
+            height - boxSize / 2,
+            width,
+            boxSize,
+            {
+                isStatic: true,
+                restitution: 1
+            }
+        );
+        const leftWall = Matter.Bodies.rectangle(2 / 1, height / 2, 2, height, {
             isStatic: true,
             restitution: 1
         });
-        const leftWall = Matter.Bodies.rectangle(2 / 1, height / 2, 2, height, {isStatic: true, restitution: 1});
-        const rightWall = Matter.Bodies.rectangle(width - 2 / 1, height / 2, 2, height, {
-            isStatic: true,
-            restitution: 1
-        });
+        const rightWall = Matter.Bodies.rectangle(
+            width - 2 / 1,
+            height / 2,
+            2,
+            height,
+            {
+                isStatic: true,
+                restitution: 1
+            }
+        );
 
         // const test = Matter.Composites.softBody(200, 200, 12, 10, 2, 2, false, 4, {
         //     friction: 0.00001, // 摩擦力
@@ -42,11 +63,10 @@ export default class RigidBodies extends Component {
         //     test.bodies[i].isStatic = true;
         // }
 
-
         const constraint = Matter.Constraint.create({
             label: "Drag Constraint",
-            pointA: {x: 0, y: 0},
-            pointB: {x: 0, y: 0},
+            pointA: { x: 0, y: 0 },
+            pointB: { x: 0, y: 0 },
             length: 0.01,
             stiffness: 0.1,
             angularStiffness: 1
@@ -59,17 +79,39 @@ export default class RigidBodies extends Component {
             <GameEngine
                 systems={[Physics, CreateBox, MoveBox, CleanBoxes]}
                 entities={{
-                    physics: {engine: engine, world: world, constraint: constraint},
-                    box: {body: body, size: [boxSize, boxSize], color: "pink", renderer: Box},
-                    floor: {body: floor, size: [width, boxSize], color: "#86E9BE", renderer: Box},
-                    leftWall: {body: leftWall, size: [2, height], color: "#86E9BE", renderer: Box},
-                    rightWall: {body: rightWall, size: [2, height], color: "#86E9BE", renderer: Box},
+                    physics: {
+                        engine: engine,
+                        world: world,
+                        constraint: constraint
+                    },
+                    box: {
+                        body: body,
+                        size: [boxSize, boxSize],
+                        color: "pink",
+                        renderer: Box
+                    },
+                    floor: {
+                        body: floor,
+                        size: [width, boxSize],
+                        color: "#86E9BE",
+                        renderer: Box
+                    },
+                    leftWall: {
+                        body: leftWall,
+                        size: [2, height],
+                        color: "#86E9BE",
+                        renderer: Box
+                    },
+                    rightWall: {
+                        body: rightWall,
+                        size: [2, height],
+                        color: "#86E9BE",
+                        renderer: Box
+                    }
                     // test: {body: test, size: [100, 100], color: "#86E9BE", renderer: Cloth}
                 }}
             >
-
-                <StatusBar hidden={true}/>
-
+                <StatusBar hidden={true} />
             </GameEngine>
         );
     }
